a2545afa42e0f144c67372f7ea699fd4e8e92d89
Jimmy Yen <zeng.tw@gmail.com>
2026-08-18 03:53:01 +0800

    Remove tskeleton test binary from skeleton plugin

--- graphify-plugin-skeleton/src/bin/tskeleton.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let path = std::env::args().nth(1).expect("Usage: tskeleton <file>");
-    match graphify_plugin_skeleton::extract_skeleton(&path) {
-        Ok(s) => println!("{}", s),
-        Err(e) => eprintln!("Error: {}", e),
-    }
-}
